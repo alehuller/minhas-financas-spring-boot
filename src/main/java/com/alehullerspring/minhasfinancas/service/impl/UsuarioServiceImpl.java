0658f64ea2bac617f46c62565187b89317f62df2
Alejandro <ale_huller@hotmail.com>
2024-05-30 16:21:43 -0300

    Criacao da Interface UsuarioService e da Classe UsuarioServiceImpl

---
+++ src/main/java/com/alehullerspring/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,34 @@
+package com.alehullerspring.minhasfinancas.service.impl;
+
+import com.alehullerspring.minhasfinancas.model.entity.Usuario;
+import com.alehullerspring.minhasfinancas.model.repository.UsuarioRepository;
+import com.alehullerspring.minhasfinancas.service.UsuarioService;
+
+public class UsuarioServiceImpl implements UsuarioService{
+	
+	private UsuarioRepository repository;
+
+	public UsuarioServiceImpl(UsuarioRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	@Override
+	public Usuario autenticar(String email, String senha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario salvarUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void validarEmail(String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
