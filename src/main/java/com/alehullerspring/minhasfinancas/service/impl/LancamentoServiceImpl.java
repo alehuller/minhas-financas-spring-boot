81923b9d330070d4f171cad22d7e74e3ff07a40e
Alejandro <ale_huller@hotmail.com>
2024-06-04 16:35:02 -0300

    Implementacao das classe LancamentoService e LancamentoServiceImpl

---
+++ src/main/java/com/alehullerspring/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,51 @@
+package com.alehullerspring.minhasfinancas.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.alehullerspring.minhasfinancas.model.entity.Lancamento;
+import com.alehullerspring.minhasfinancas.model.enums.StatusLancamento;
+import com.alehullerspring.minhasfinancas.model.repository.LancamentoRepository;
+import com.alehullerspring.minhasfinancas.service.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService{
+	
+	private LancamentoRepository repository;
+	
+	public LancamentoServiceImpl(LancamentoRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public Lancamento salvar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Lancamento atualizar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
