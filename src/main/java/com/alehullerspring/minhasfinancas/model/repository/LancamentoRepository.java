d96a34cf2c4c11a258c80b1a14ea1efb7ab8fb37
Alejandro <ale_huller@hotmail.com>
2024-06-06 16:34:16 -0300

    Arrumando a query de buscar saldo

--- src/main/java/com/alehullerspring/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/alehullerspring/minhasfinancas/model/repository/LancamentoRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.alehullerspring.minhasfinancas.model.entity.Lancamento;
+import com.alehullerspring.minhasfinancas.model.enums.TipoLancamento;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
 
-	@Query(value = "select sum(l.valor) from lancamento l join l.usuario u where u.id = :idUsuario and l.tipo = :tipo group by u")
-	BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long id, @Param("tipo") String tipo);
+	@Query(value = "select sum(l.valor) from Lancamento l join l.usuario u where u.id = :idUsuario and l.tipo = :tipo group by u")
+	BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long id, @Param("tipo") TipoLancamento tipo);
 }
