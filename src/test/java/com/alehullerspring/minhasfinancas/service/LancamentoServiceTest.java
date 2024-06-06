45080448a2deae9683105a685ef319628cbd6bbf
Alejandro <ale_huller@hotmail.com>
2024-06-06 18:51:12 -0300

    Implementando o teste de filtrar lancamento

--- src/test/java/com/alehullerspring/minhasfinancas/service/LancamentoServiceTest.java
+++ src/test/java/com/alehullerspring/minhasfinancas/service/LancamentoServiceTest.java
@@ -1,11 +1,15 @@
 package com.alehullerspring.minhasfinancas.service;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.domain.Example;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -92,5 +96,18 @@ public class LancamentoServiceTest {
 		Assertions.catchThrowableOfType( () -> service.deletar(lancamento), NullPointerException.class);
 		
 		Mockito.verify(repository, Mockito.never()).delete(lancamento);
+	}
+	
+	@Test
+	public void deveFiltrarLancamento() {
+		Lancamento lancamento = LancamentoRepositoryTest.criarLancamento();
+		lancamento.setId(1l);
+		
+		List<Lancamento> lista = Arrays.asList(lancamento);
+		Mockito.when(repository.findAll(Mockito.any(Example.class))).thenReturn(lista);
+		
+		List<Lancamento> resultado = service.buscar(lancamento);
+		
+		Assertions.assertThat(resultado).isNotEmpty().hasSize(1).contains(lancamento);
 	}
 }
